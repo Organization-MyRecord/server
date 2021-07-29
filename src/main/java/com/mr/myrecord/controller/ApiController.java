29c479c3c80c804d41ae4032de0bfc917343e6c5
wlrhkd49 <wlrhkd49@naver.com>
2021-07-29 16:51:55 +0900

    test

--- src/main/java/com/mr/myrecord/controller/ApiController.java
+++ src/main/java/com/mr/myrecord/controller/ApiController.java
@@ -1,4 +1,14 @@
 package com.mr.myrecord.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiController {
+
+    @GetMapping("/api")
+    public String hello() {
+        return "hello";
+    }
+
 }
