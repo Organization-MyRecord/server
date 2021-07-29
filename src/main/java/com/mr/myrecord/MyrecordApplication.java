2d8c3879303e7dac5bc1be9128abe985594dd08b
wlrhkd49 <wlrhkd49@naver.com>
2021-07-29 16:05:52 +0900

    init spring boot

---
+++ src/main/java/com/mr/myrecord/MyrecordApplication.java
@@ -0,0 +1,13 @@
+package com.mr.myrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyrecordApplication.class, args);
+    }
+
+}
